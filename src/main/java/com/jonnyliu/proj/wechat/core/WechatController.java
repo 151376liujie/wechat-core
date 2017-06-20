d1356af396b82aee29d2a133962c8a228098c20e
jonnyliu <980463316@qq.com>
2017-06-20 10:00:00 +0800

    去除多余包

--- src/main/java/com/jonnyliu/proj/wechat/core/WechatController.java
+++ src/main/java/com/jonnyliu/proj/wechat/core/WechatController.java
@@ -16,14 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.ContextLoader;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
 import java.util.Map;
 
 /**
